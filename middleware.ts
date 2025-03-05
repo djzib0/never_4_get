72710c8fe8928bc327d70e1d249390dfead02313
djzib0 <piotrsabatura@gmail.com>
2025-03-05 19:17:51 +0100

    test: change middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,18 @@
-export { auth as middleware } from "@/auth"
+// export { auth as middleware } from "@/auth"
+import NextAuth from "next-auth";
+import { authConfig} from "./lib/auth.config"
+
+export default NextAuth(authConfig).auth;
+
+
+export const config = {
+    matcher: [
+      '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
+    unstable_allowDynamic: [
+        // allows a single file
+        // "/src/db/lib/dbConnect.js",
+        // use a glob to allow anything in the function-bind 3rd party module
+        "/node_modules/mongoose/dist/**",
+    ],
+  }
